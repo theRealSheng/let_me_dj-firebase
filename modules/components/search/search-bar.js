2ab5f7c7d0e41ebef3fb49e1833b62e50c19adea
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-09 19:34:00 +0300

    Finish Search Bar component

--- modules/components/search/search-bar.js
+++ modules/components/search/search-bar.js
@@ -1,11 +1,18 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, TextInput } from 'react-native';
 
 class SearchBar extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {term: ''};
+    }
   render() {
     return (
       <View>
-        
+        <TextInput
+            value={this.state.term}
+            onChange={(term) => this.setState({term})}
+        />
       </View>
     );
   }
