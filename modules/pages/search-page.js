33de0c837d1a94204edaf94b2ec4448525cb6836
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-09 19:41:14 +0300

    connect search page to api and search bar

--- modules/pages/search-page.js
+++ modules/pages/search-page.js
@@ -1,11 +1,34 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import YTSearch from 'youtube-api-search'
+import SearchBar from '../components/search/search-bar';
+
+const API_KEY = '';
 
 class SearchPage extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    }
+  }
+
+  videoSearch(term) {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
+      this.setState({
+        videos: videos,
+        selectedVideo: videos[0]
+      });
+    });
+  }
+
   render() {
     return (
       <View>
-        
+        <SearchBar
+          onSearchTermChange={(term) => this.videoSearch(term)}
+        />
       </View>
     );
   }
