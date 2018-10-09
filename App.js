7bb86a43a4ec91434c0e19be19630673f154ea1c
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-09 19:00:41 +0300

    Initial commit in dev front

--- App.js
+++ App.js
@@ -22,9 +22,6 @@ export default class App extends Component<Props> {
   render() {
     return (
       <View style={styles.container}>
-        <Text style={styles.welcome}>Welcome to React Native!</Text>
-        <Text style={styles.instructions}>To get started, edit App.js</Text>
-        <Text style={styles.instructions}>{instructions}</Text>
       </View>
     );
   }
