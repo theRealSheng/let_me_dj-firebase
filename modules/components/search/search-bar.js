9662dcdb31fbab0e0a78e8542331a5f174889ba0
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-09 19:21:31 +0300

    Create search bar component and install youtube search api

---
+++ modules/components/search/search-bar.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { View } from 'react-native';
+
+class SearchBar extends Component {
+  render() {
+    return (
+      <View>
+        
+      </View>
+    );
+  }
+}
+
+export default SearchBar;
