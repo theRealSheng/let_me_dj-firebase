66439c8f0f04869492f6835c886e2bfa69effa75
Rasbin Rijal <rasbin.rijal@gmail.com>
2018-10-09 18:57:51 +0300

    Initial Commit

---
+++ components/auth.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react'
+import {Text, TextInput, View} from 'react-native'
+
+class Auth extends Component {
+    render() {
+        return (
+            <view>
+                
+            </view>
+        );
+    }
+}
+
+export default Auth
