fc47cf01420ab383cb06d115e15aa8b243790219
theRealSheng <sheng.huchai@gmail.com>
2018-10-10 23:05:23 +0300

    Dj-pag: connect to firebase

--- modules/pages/dj-page.js
+++ modules/pages/dj-page.js
@@ -1,28 +1,31 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import DjSeat from './../components/dj/dj-seat';
+import firebase from 'firebase';
+import { createStackNavigator } from "react-navigation";
+
 
 class DjPage extends Component {
+  static navigationOptions = {
+    title: "Dj"
+  };
+
   constructor(props) {
     super(props);
 
-    // this.state = {
-    //  roomDjs: '',
-    //  roomCode: '',
-    //  currentUser: ''
-    // }
+    this.state = {
+      currentUser: JSON.stringify(this.props.navigation.getParam('currentUser', '')),
+      roomId: JSON.stringify(this.props.getParam('room', '')),
+      roomPeople: ''
+    }
   }
 
   // check if it works
   componentDidlMount() {
-    fetch('url')
-      .then(() => res.json())
-      .then((chatRoom) => {
-        // this.setState({chatRoom})
-      })
-      .catch((err) => {
-        console.log(err.message);
-      })
+    const roomId = this.state.roomId;
+    firebase.database().ref(`room/${roomId}`).on('value', (data) => {
+      this.setState({roomPeople: data.people});
+    })
   }
 
 
