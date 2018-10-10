4f8ac2e778445c6947f0a69e375510282a8eb44a
theRealSheng <sheng.huchai@gmail.com>
2018-10-10 11:48:22 +0300

    dj-seat: import StyleSheet

--- modules/components/dj/dj-seat.js
+++ modules/components/dj/dj-seat.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { View } from 'react-native';
+import { View, StyleSheet } from 'react-native';
 
 class DjSeat extends Component {
   constructor(props) {
