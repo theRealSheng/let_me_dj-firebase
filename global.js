4a74facb84378eaaa6f0a268cd400489d2b30153
theRealSheng <sheng.huchai@gmail.com>
2018-10-10 18:17:40 +0300

    Add global file and navigation npm

---
+++ global.js
@@ -0,0 +1,9 @@
+
+var config = {
+  apiKey: "AIzaSyDiROfE0E83mxIz2V3CDUsMmi0pO4u2lLc",
+  authDomain: "let-me-dj.firebaseapp.com",
+  databaseURL: "https://let-me-dj.firebaseio.com",
+  projectId: "let-me-dj",
+  storageBucket: "let-me-dj.appspot.com",
+  messagingSenderId: "59693446433"
+};
