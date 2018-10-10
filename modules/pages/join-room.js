dbc1fc3003b198f15110f182fc0a748ddcda41bd
theRealSheng <sheng.huchai@gmail.com>
2018-10-10 20:15:14 +0300

    Add to join room

--- modules/pages/join-room.js
+++ modules/pages/join-room.js
@@ -10,119 +10,135 @@ import {
 } from "react-native";
 import User from "./user";
 
-const instructions = Platform.select({
-  ios: "Press Cmd+R to reload,\n" + "Cmd+D or shake for dev menu",
-  android:
-    "Double tap R on your keyboard to reload,\n" +
-    "Shake or press menu button for dev menu"
-});
+class JoinRoom extends Component {
 
-export default class joinRoom extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { words: [] };
-    this.inputRef = React.createRef();
-    this.inputText = "";
-  }
+ state = {
+   currentUser: '',
+   room: ''
+ }
 
-  onPressButton() {
-    // POST to Create
-    console.log(this.state.text);
-    axios
-      .post("server_url/route", {
-        createRoom: this.state.text
-      })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
 
-  onClickButton() {
-    // POST to Join
-    console.log(this.state.text);
-    axios
-      .post("server_url/route", {
-        joinRoom: this.state.text
-      })
-      .then(function(response) {
-        console.log(response);
-      })
-      .catch(function(error) {
-        console.log(error);
-      });
-  }
+ async componentDidMount() {
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text style={styles.headerText}>Let Me DJ!</Text>
-        <Text style={styles.dialogText}>
-          Create a room or join existing one!
-        </Text>
-        <Text style={styles.dialogText}>Enter four digits</Text>
+   const config = {
+     apiKey: "AIzaSyDiROfE0E83mxIz2V3CDUsMmi0pO4u2lLc",
+     authDomain: "let-me-dj.firebaseapp.com",
+     databaseURL: "https://let-me-dj.firebaseio.com",
+     projectId: "let-me-dj",
+     storageBucket: "let-me-dj.appspot.com",
+     messagingSenderId: "59693446433"
+   };
 
-        <View style={styles.inputPlace}>
-          <TextInput
-            value={this.state.text}
-            maxLength={4}
-            style={styles.input}
-            autoCapitalize="characters"
-            placeholder={"Create a Room"}
-          />
-          <Button onPress={() => this.send.onPressButton()} title={"Create"} />
-        </View>
+   firebase.initializeApp(config);
 
-        <View style={styles.inputPlace}>
-          <TextInput
-            value={this.state.text}
-            maxLength={4}
-            style={styles.input}
-            autoCapitalize="characters"
-            placeholder={"Join Room"}
-          />
-          <Button onPress={() => this.send.onClickButton()} title={"Join"} />
-        </View>
-      </View>
-    );
-  }
+   this.state.currentUser = await AsyncStorage.getItem('currenUser');
+
+ }
+
+ onPressCreate = () => {
+   const currentUser = this.state.currentUser;
+   const room = this.state.text;
+
+   firebase.database().ref('roomID')
+     .set({room: room, people: currentUser})
+     .then((result) => {
+       console.log('THE ROOM RESULT IS...', result);
+       this.setState({room: result.key})
+       // REroute to create and go to Room page
+       alert('Created new room');
+     })
+     .catch((err) => {
+       console.log(err);
+     })
+ }
+
+ onChangesRoom = (room) => {
+   this.setState({room})
+ }
+
+ onPressJoin() {
+   // POST to Join
+   firebase.database().ref('currentID')
+     .push(this.state.text)
+     .then((result) => {
+       this.setState({currentUser: result.key})
+       // REroute to Room page
+       alert('Joined new room');
+     })
+     .catch((err) => {
+       console.log(err);
+     })
+ }
+
+ render() {
+   return (
+     <View style={styles.container}>
+       <Text style={styles.headerText}>Let Me DJ!</Text>
+       <Text style={styles.dialogText}>
+         Create a room or join existing one!
+       </Text>
+       <Text style={styles.dialogText}>Enter four digits</Text>
+
+       <View style={styles.inputPlace}>
+         <TextInput
+           onChangeText={(text) => this.onChangesRoom(text)}
+           value={this.state.room}
+           maxLength={4}
+           style={styles.input}
+           autoCapitalize="characters"
+           placeholder={"Create a Room"}
+         />
+         <Button onPress={() => this.onPressCreate} title={"Create"} />
+       </View>
+
+       <View style={styles.inputPlace}>
+         <TextInput
+           value={this.state.room}
+           maxLength={4}
+           style={styles.input}
+           autoCapitalize="characters"
+           placeholder={"Join Room"}
+         />
+         <Button onPress={() => this.props.onPressCreate()} title={"Join"} />
+       </View>
+     </View>
+   );
+ }
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    flexDirection: "column",
-    alignItems: "center",
-    alignSelf: "stretch",
-    justifyContent: "flex-start",
-    margin: 10,
-    paddingTop: 60,
-    backgroundColor: "white"
-  },
-  inputPlace: {
-    padding: 20,
-    alignSelf: "stretch"
-  },
+ container: {
+   flex: 1,
+   flexDirection: "column",
+   alignItems: "center",
+   alignSelf: "stretch",
+   justifyContent: "flex-start",
+   margin: 10,
+   paddingTop: 60,
+   backgroundColor: "white"
+ },
+ inputPlace: {
+   padding: 20,
+   alignSelf: "stretch"
+ },
 
-  input: {
-    height: 50,
-    alignSelf: "stretch",
-    alignContent: "center",
-    fontSize: 20
-  },
-  headerText: {
-    fontSize: 32
-  },
-  dialogText: {
-    fontSize: 16,
-    padding: 5
-  },
-  text: {
-    marginTop: 20,
-    fontSize: 24
-  }
+ input: {
+   height: 50,
+   alignSelf: "stretch",
+   alignContent: "center",
+   fontSize: 20
+ },
+ headerText: {
+   fontSize: 32
+ },
+ dialogText: {
+   fontSize: 16,
+   padding: 5
+ },
+ text: {
+   marginTop: 20,
+   fontSize: 24
+ }
 });
 
-export default joinRoom;
+export default JoinRoom;
