fbda43a6870a15a7bbc50b2a45a1fc515cdacf49
theRealSheng <sheng.huchai@gmail.com>
2018-10-10 15:57:39 +0300

    App: Pushing methods for firebase

--- App.js
+++ App.js
@@ -1,23 +1,69 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import SearchPage from './modules/pages/search-page';
-import DjPage from './modules/pages/dj-page';
-// https://github.com/julianocomg/react-native-form
-
-const instructions = Platform.select({
-  ios: 'Press Cmd+R to reload,\n' + 'Cmd+D or shake for dev menu',
-  android:
-    'Double tap R on your keyboard to reload,\n' +
-    'Shake or press menu button for dev menu',
-});
+// import SearchPage from './modules/pages/search-page';
+// import DjPage from './modules/pages/dj-page';
+import Auth from './modules/pages/auth';
+import firebase from 'firebase';
+import conf from './global'
 
 export default class App extends Component {
+
+  componentWillmount() {
+    var config = {
+      apiKey: "AIzaSyDiROfE0E83mxIz2V3CDUsMmi0pO4u2lLc",
+      authDomain: "let-me-dj.firebaseapp.com",
+      databaseURL: "https://let-me-dj.firebaseio.com",
+      projectId: "let-me-dj",
+      storageBucket: "let-me-dj.appspot.com",
+      messagingSenderId: "59693446433"
+    };
+
+    firebase.initializeApp(config);
+
+  //   console.log(firebase);
+
+  //   // Create data
+  //   firebase.database().ref('users/001').set(
+  //     {
+  //       name: 'Rasbin user',
+  //       age: 28
+  //     }
+  //   ).then(() => {
+  //     console.log(' Inserted...')
+  //   })
+  //   .catch((err) => {
+  //     console.log(err);
+  //   })
+
+  //   // Select data - on = when changes once = called once
+  //   firebase.databse().ref('users').on('value', (data) => {
+  //     console.log(data.toJSON());
+  //   })
+
+  //   // Update information
+  //   firebase.database().ref('users/004').update({
+  //     name: ' Sheng'
+  //   })
+
+  //   // Remove data - Delete last folder/item name
+  //   firebase.database().ref('users/004/name').remove()
+    }
+
+  onSubmitNewUser = (name) => {
+    firebase.database().ref('users').push({name})
+    .then(() => {
+      alert('added new user');
+    })
+    .catch((err) => {
+      console.log(err);
+    }) 
+  }
+
   render() {
     return (
       <View style={styles.container}>
         <Text>Welcome Dj!</Text>
-        <SearchPage />
-        {/* <DjPage /> */}
+        <Auth onSubmitUser={this.onSubmitNewUser} />
       </View>
     );
   }
@@ -28,7 +74,6 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
-    backgroundColor: '#F5FCFF',
   },
   welcome: {
     fontSize: 20,
